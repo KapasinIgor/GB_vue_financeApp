449bd965451723ed44efd261747871c6c4d12890
KapasinIgor <kapasin.igor@gmail.com>
2022-05-25 15:43:10 +0300

    work on the tasks of lesson8, added vuetify lib

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-app>
+    <v-app-bar app flat>
+      <v-btn plain :ripple="false" to="/dashboard">Дашборд</v-btn>
+      <v-btn plain :ripple="false" to="/about">О приложении</v-btn>
+      <v-btn class="red--text" plain :ripple="false" to="/404">не нажимать!</v-btn>
+    </v-app-bar>
+    <v-main>
+      <router-view/>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+
+export default {
+
+}
+
+</script>
+
+<style lang="scss">
+
+</style>
